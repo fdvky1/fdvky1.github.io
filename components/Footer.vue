9ce30a2c8284ce25d9157537aa8f80f21f7a5454
Fdvky1 <fdvkey@gmail.com>
2024-06-09 08:01:17 +0700

    refactor: using pinia

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-    <footer class="hidden sm:block -mx-4">
+    <footer class="hidden sm:block fixed w-[calc(100vw-1.5rem)] z-[701] -ml-4">
         <div class="flex items-center justify-between text-gray-400">
             <div class="flex items-center gap-2">
                 <div class="flex items-center gap-1 bg-gray-800 pr-1 rounded-bl-md overflow-hidden">
@@ -7,12 +7,12 @@
                     <span>Visual</span>
                 </div>
                 <div class="flex items-center gap-1 bg-gray-800 pr-1">
-                    <svg stroke="currentColor" fill="currentColor" stroke-width="0" role="img" viewBox="0 0 24 24" class="w-[30px] h-[30px] p-2 bg-[#659b8f] text-[#4f4e57]" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M1.00098 3H5.00098L12.001 15L19.001 3H23.001L12.001 22L1.00098 3ZM9.66764 3L12.001 7L14.3343 3H18.3694L12.001 14L5.63256 3H9.66764Z"></path></svg>
-                    <span>index.vue</span>
+                    <svg stroke="currentColor" fill="currentColor" stroke-width="0" role="img" viewBox="0 0 24 24" class="w-[30px] h-[30px] p-2" :style="`background-color: ${navigationState.selectedFile.bgColor}; color: ${navigationState.selectedFile.textColor};`" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg" v-html="svgPath[navigationState.selectedFile.ext]"></svg>
+                    <span>{{ navigationState.selectedFile.name }}</span>
                 </div>
 
                 <div class="flex items-center gap-1 pr-1">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="12" height="12" fill="rgba(79,78,87,1)"><path d="M7.10508 15.2101C8.21506 15.6501 9 16.7334 9 18C9 19.6569 7.65685 21 6 21C4.34315 21 3 19.6569 3 18C3 16.6938 3.83481 15.5825 5 15.1707V8.82929C3.83481 8.41746 3 7.30622 3 6C3 4.34315 4.34315 3 6 3C7.65685 3 9 4.34315 9 6C9 7.30622 8.16519 8.41746 7 8.82929V11.9996C7.83566 11.3719 8.87439 11 10 11H14C15.3835 11 16.5482 10.0635 16.8949 8.78991C15.7849 8.34988 15 7.26661 15 6C15 4.34315 16.3431 3 18 3C19.6569 3 21 4.34315 21 6C21 7.3332 20.1303 8.46329 18.9274 8.85392C18.5222 11.2085 16.4703 13 14 13H10C8.61653 13 7.45179 13.9365 7.10508 15.2101ZM6 17C5.44772 17 5 17.4477 5 18C5 18.5523 5.44772 19 6 19C6.55228 19 7 18.5523 7 18C7 17.4477 6.55228 17 6 17ZM6 5C5.44772 5 5 5.44772 5 6C5 6.55228 5.44772 7 6 7C6.55228 7 7 6.55228 7 6C7 5.44772 6.55228 5 6 5ZM18 5C17.4477 5 17 5.44772 17 6C17 6.55228 17.4477 7 18 7C18.5523 7 19 6.55228 19 6C19 5.44772 18.5523 5 18 5Z"></path></svg>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="12" height="12"  fill="rgba(243, 250, 250, 1)"><path d="M7.10508 15.2101C8.21506 15.6501 9 16.7334 9 18C9 19.6569 7.65685 21 6 21C4.34315 21 3 19.6569 3 18C3 16.6938 3.83481 15.5825 5 15.1707V8.82929C3.83481 8.41746 3 7.30622 3 6C3 4.34315 4.34315 3 6 3C7.65685 3 9 4.34315 9 6C9 7.30622 8.16519 8.41746 7 8.82929V11.9996C7.83566 11.3719 8.87439 11 10 11H14C15.3835 11 16.5482 10.0635 16.8949 8.78991C15.7849 8.34988 15 7.26661 15 6C15 4.34315 16.3431 3 18 3C19.6569 3 21 4.34315 21 6C21 7.3332 20.1303 8.46329 18.9274 8.85392C18.5222 11.2085 16.4703 13 14 13H10C8.61653 13 7.45179 13.9365 7.10508 15.2101ZM6 17C5.44772 17 5 17.4477 5 18C5 18.5523 5.44772 19 6 19C6.55228 19 7 18.5523 7 18C7 17.4477 6.55228 17 6 17ZM6 5C5.44772 5 5 5.44772 5 6C5 6.55228 5.44772 7 6 7C6.55228 7 7 6.55228 7 6C7 5.44772 6.55228 5 6 5ZM18 5C17.4477 5 17 5.44772 17 6C17 6.55228 17.4477 7 18 7C18.5523 7 19 6.55228 19 6C19 5.44772 18.5523 5 18 5Z"></path></svg>
                     <nuxt-link to="https://github.com/fdvky1/fdvky1" target="_blank">main</nuxt-link>
                 </div>
             </div>
@@ -26,3 +26,11 @@
         </div>
     </footer>
 </template>
+
+
+<script setup lang="ts">
+import { useNavigationStore, svgPath } from '~/stores/navigation';
+
+const navigationState = useNavigationStore();
+
+</script>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="min-h-[100dvh] w-full p-3 bg-[url('/images/26796717_2203_w026_n002_1490b_p1_1490.jpg')] bg-no-repeat bg-cover">
-        <div class="p-4 w-full h-[calc(100dvh-1.5rem)] bg-neutral-950 rounded-md bg-opacity-85">
+        <div class="p-4 w-full h-[calc(100dvh-1.5rem)] bg-neutral-950 rounded-md bg-opacity-80">
             <Nav/>
             <div class="h-[calc(100dvh-6.5rem)] w-full">
                 <slot/>
@@ -9,3 +9,45 @@
         </div>
     </div>
 </template>
+
+<script setup lang="ts">
+import { useNavigationStore } from '~/stores/navigation';
+
+const router = useRouter();
+const navigationState = useNavigationStore();
+
+const fixFilename = (path: string) => {
+    switch (path){
+        case '/':
+            navigationState.setState({
+                name: "index.vue",
+                ext: "vue",
+                bgColor: "#659b8f",
+                textColor: "#4f4e57"
+            })
+            break;
+        case '/about':
+            navigationState.setState({
+                name: "personal.ts",
+                ext: "ts",
+                bgColor: "#4d82c8",
+                textColor: "#4f4e57"
+            })
+            break;
+        case '/projects':
+            break;
+        case '/contacts':
+            break;
+        default:
+            break;
+    }
+}
+
+onMounted(() => {
+    fixFilename(router.currentRoute.value.path)
+});
+
+watch(router.currentRoute, ()=> {
+    fixFilename(router.currentRoute.value.path)
+})
+</script>
